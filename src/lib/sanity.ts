5bd2ce1c2802169e979fb36083587589cf93848d
jjrock98 <clarosjavier1998@gmail.com>
2026-05-07 17:29:37 -0300

    actualizacion

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -2,7 +2,7 @@ import { createClient } from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 import type { SanityImageSource } from '@sanity/image-url';
 
-const projectId = import.meta.env.VITE_SANITY_PROJECT_ID || 'hwujeebe';
+const projectId = import.meta.env.VITE_SANITY_PROJECT_ID || 'txli48cz';
 const dataset = import.meta.env.VITE_SANITY_DATASET || 'production';
 const apiVersion = import.meta.env.VITE_SANITY_API_VERSION || '2024-01-01';
 
